440040aa2681a4142ca0caf893404af7d342c9fe
inancalp <inancgunalp@gmail.com>
2021-12-31 11:42:57 +0100

    Queues: change the functions to your owns!

--- main.py
+++ main.py
@@ -1,306 +1,73 @@
-color_red = '\033[91m'
-color_white = '\033[0m'
-
-class Node():
-    def __init__(self, data, next_node=None):
-        self.data = data
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
         self.next_node = next_node
 
+    def set_next_node(self, next_node):
+        self.next_node = next_node
 
-class LinkedList():
-    def __init__(self, head_node=None):
-        self.head_node = head_node
+    def get_next_node(self):
+        return self.next_node
 
-    def insert_beginning(self, value):
-
-        new_node = Node(value)
-        new_node.next_node = self.head_node
-        self.head_node = new_node
+    def get_value(self):
+        return self.value
 
 
-    def insert_end(self, value):
+class Queue:
+    def __init__(self, max_size=None):
+        self.head = None
+        self.tail = None
+        self.max_size = max_size
+        self.size = 0
 
-        if self.head_node is None:
-            self.head_node = Node(value)
+    def enqueue(self, value):
+        if self.has_space():
+            item_to_add = Node(value)
+            print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+            if self.is_empty():
+                self.head = item_to_add
+                self.tail = item_to_add
+            else:
+                self.tail.set_next_node(item_to_add)
+                self.tail = item_to_add
+            self.size += 1
         else:
-            current_node = self.head_node
-            while current_node.next_node is not None:
-                current_node = current_node.next_node
-            current_node.next_node = Node(value)
+            print("Sorry, no more room!")
 
-    def print_list(self):
-        str_list = ''
-        current_node = self.head_node
-        while current_node is not None:
-            if current_node.next_node is not None:
-                if type(current_node.data) is str:
-                    str_list += f'"{current_node.data}", '
-                    current_node = current_node.next_node
-                else:
-                    str_list += f'{current_node.data}, '
-                    current_node = current_node.next_node
+    # Add your dequeue method below:
+    def dequeue(self):
+        if self.size != 0:
+            item_to_remove = self.head
+            print('Removing', str(item_to_remove.get_value()), 'from the queue!')
+            if self.size == 1:
+                self.head = None
+                self.tail = None
             else:
-                if type(current_node.data) is str:
-                    str_list += f'"{current_node.data}"'
-                    current_node = current_node.next_node
-                else:
-                    str_list += f'{current_node.data}'
-                    current_node = current_node.next_node
-        str_list = f'[{str_list}]'
-        return str_list
-
-    def get_length(self):
-        count = 0
-        node = self.head_node
-        while node:
-            count += 1
-            node = node.next_node
-        return count
-
-    def remove_data(self, value):
-        current_node = self.head_node
-        try:
-            if current_node.data == value:
-                self.head_node = current_node.next_node
-            else:
-                while current_node:
-                    if current_node.next_node.data != value:
-                        current_node = current_node.next_node
-                    else:
-                        current_node.next_node = current_node.next_node.next_node
-                        break
-        except AttributeError:
-            # raise AttributeError
-            print(color_red + 'Value does not Exist!!' + color_white)
-
-
-    def remove_index(self, index):
-        current_node = self.head_node
-        count = 0
-        if index < 0 or index >= ll.get_length():
-            print(color_red + 'Invalid Index Number!' + color_white)
-        elif index == 0:
-            self.head_node = self.head_node.next_node
+                self.head = self.head.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
         else:
-            while index != count:
-                count += 1
-                if index == count:
-                    current_node.next_node = current_node.next_node.next_node
-                    break
-                else:
-                    current_node = current_node.next_node
+            print('This queue is totally empty!')
 
-
-    def insert_at(self, index, data):
-        node = Node(data)
-        current_node = self.head_node
-        count = 0
-        if index < 0 or index > ll.get_length():
-            print(color_red + 'Invalid Index Number!' + color_white)
-        elif index == 0:
-            node.next_node = current_node
-            self.head_node = node
-        elif index == ll.get_length():
-            ll.insert_end(data)
-
+    def peek(self):
+        if self.is_empty():
+            print("Nothing to see here!")
         else:
-            while True:
-                count += 1
-                if count == index:
-                    node.next_node = current_node.next_node
-                    current_node.next_node = node
-                    break
-                else:
-                    current_node = current_node.next_node
+            return self.head.get_value()
 
-    def insert_after_value(self, data_after, data_to_insert):
-        current_node = self.head_node
-        node_to_insert = Node(data_to_insert)
-        while True:
-            if current_node.data == data_after:
-                node_to_insert.next_node = current_node.next_node
-                current_node.next_node = node_to_insert
-                break
-            else:
-                current_node = current_node.next_node
+    def get_size(self):
+        return self.size
 
-    def remove_by_value(self, data):
-        current_node = self.head_node
-        if current_node.data == data:
-            current_node = current_node.next_node
-        while True:
-            if current_node.next_node.data == data:
-                current_node.next_node = current_node.next_node.next_node
-                break
-            else:
-                current_node = current_node.next_node
-
-    def swap_nodes(self, val1, val2):
-
-        if val1 == val2:
-            print('values are same!')
-            return
-
-        node1 = self.head_node
-        node2 = self.head_node
-        node1_prev = None
-        node2_prev = None
-
-        while True:
-            if node1.data == val1:
-                break
-            else:
-                node1_prev = node1
-                node1 = node1.next_node
-                if node1 is None:
-                    print(color_red + 'val1 is not in the list!, execution is about to stop.' + color_white)
-                    return
-        while True:
-            if node2.data == val2:
-                break
-            else:
-                node2_prev = node2
-                node2 = node2.next_node
-                if node2 is None:
-                    print(color_red + 'val2 is not in the list!, execution is about to stop' + color_white)
-                    return
-
-
-
-        if node1_prev is None:
-            self.head_node = node2
+    def has_space(self):
+        if self.max_size == None:
+            return True
         else:
-            node1_prev.next_node = node2
+            return self.max_size > self.get_size()
 
-        if node2_prev is None:
-            self.head_node = node1
-        else:
-            node2_prev.next_node = node1
-
-        temp = node1.next_node
-        node1.next_node = node2.next_node
-        node2.next_node = temp
+    def is_empty(self):
+        return self.size == 0
 
 
-
-
-
-
-ll = LinkedList()
-ll.insert_at(0, 15)
-ll.insert_at(0, 20)
-ll.insert_at(1, 'hello')
-# print(ll.print_list())
-ll.insert_after_value(15, 'HI')
-# print(ll.print_list())
-
-ll.insert_beginning('inanc alp')
-
-ll.insert_after_value('HI', 27)
-
-print(ll.print_list())
-
-print('----------')
-
-ll.swap_nodes(20, 15)
-
-print(ll.print_list())
-
-ll.swap_nodes(12, 13)
-
-print(ll.print_list())
-
-
-
-
-
-
-
-
-
-
-
-#
-# a = Node(1)
-# b = Node(2, a)
-# c = Node(3, b)
-# d = Node(4, c)
-# ll = LinkedList(d)
-#
-# ll.insert_end(10)
-# ll.insert_end(12)
-# ll = LinkedList()
-# ll.insert_at(0, 15)
-# ll.insert_at(0, 20)
-# ll.insert_at(1, 'hello')
-# print(ll.print_list())
-
-
-
-# print(ll.print_list())
-#
-# ll.insert_beginning(31)
-#
-# print(ll.print_list())
-#
-#
-# items = ['inanc', 'alp', 'gunalp']
-#
-# for item in items:
-#     ll.insert_end(item)
-#
-# print(ll.print_list())
-#
-# print(ll.get_length())
-#
-# ll.insert_end('sahra simay gunalp')
-#
-# print(ll.print_list())
-#
-# print('lenght:', ll.get_length())
-#
-# ll.remove_w_index(-1)
-#
-# print(ll.print_list())
-#
-# print('lenght:', ll.get_length())
-
-# print('------------')
-#
-# print(ll.remove_data(31))
-#
-# print(ll.print_list())
-#
-#
-# print(ll.remove_data('inanc'))
-#
-# print(ll.print_list())
-#
-#
-# print(ll.head_node.data)
-#
-# print('----------')
-#
-# node1 = ll.head_node
-#
-# print(type(node1.data))
-# print(node1.data)
-#
-# while True:
-#     try:
-#         if type(node1.data) is int:
-#             ll.remove_data(node1.data)
-#             node1 = node1.next_node
-#         else:
-#             node1 = node1.next_node
-#     except AttributeError:
-#         break
-#
-#
-# print(ll.print_list())
-
-
-
-
-
-
+q = Queue()
+q.enqueue("some guy with a mustache")
+q.dequeue()
